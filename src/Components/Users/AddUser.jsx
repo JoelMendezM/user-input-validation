c363226240a62f05ddbe933f5905d08c8d2d264a
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-02-02 10:16:36 -0300

    *Adding Button Component *Adding Card component as a style wraper *Adding User form logic

---
+++ src/Components/Users/AddUser.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    } 
+    //Adding "+" in front of the variable transform it into a number
+    if (+enteredAge < 0) {
+      return;
+    }
+
+    const userInput = {
+      username: enteredUsername,
+      age: enteredAge,
+    };
+
+    props.onSaveUserInput(userInput);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="userage">Age (Years)</label>
+        <input
+          id="userage"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
